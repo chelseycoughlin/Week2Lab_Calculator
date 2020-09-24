8f4b941d4f250ba7cc1e50fa5a33ab55da0343b0
813017 <813017@SAIT225640IT.ACDM.DS.SAIT.CA>
2020-09-24 14:25:05 -0600

    Inital configuration

--- src/java/servlets/ArithmeticCalculatorServlet.java
+++ src/java/servlets/ArithmeticCalculatorServlet.java
@@ -2,12 +2,18 @@ package servlets;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class AgeCalculatorServlet extends HttpServlet {
-    @Override
+/**
+ *
+ * @author 813017
+ */
+@WebServlet(urlPatterns = {"/ArithmeticCalculatorServlet"})
+public class ArithmeticCalculatorServlet extends HttpServlet {
+   @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         getServletContext().getRequestDispatcher("/WEB-INF/helloWorldJSP.jsp").forward(request, response);
